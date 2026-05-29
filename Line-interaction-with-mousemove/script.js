a8078ef54999b6716bf076510e4728d1654e6814
Sajal Singh <as3199432@gmail.com>
2026-05-29 22:07:22 +0530

    complete

---
+++ Line-interaction-with-mousemove/script.js
@@ -0,0 +1,20 @@
+var finalPosition = "M 100 250 Q 1000 250 2200 250"
+var svg = document.querySelector(".string")
+
+svg.addEventListener("mousemove", function (dets) {
+    path = `M 100 250 Q ${dets.x} ${dets.y} 2200 250`
+    gsap.to("svg path", {
+        attr: { d: path },
+        duration : 0.1,
+        ease : "power3.out"
+    })
+})
+
+
+svg.addEventListener("mouseleave", function () {
+    gsap.to("svg path", {
+        attr: { d: finalPosition },
+        duration: 1.5,
+        ease:"elastic.out(1,0.1)"
+    })
+})
