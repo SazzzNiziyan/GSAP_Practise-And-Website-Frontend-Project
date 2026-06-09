98ebdbca749613ed2252675b78cab795ec46f688
Sajal Singh <as3199432@gmail.com>
2026-06-09 22:49:15 +0530

    do formating of code to increase readibility

--- gsap-menu/script.js
+++ gsap-menu/script.js
@@ -3,30 +3,30 @@ var opon = document.querySelector(".ri-menu-3-line")
 var close = document.querySelector(".ri-close-large-line")
 var tl = gsap.timeline()
 
-tl.to(menu,{
+tl.to(menu, {
     right: "0%",
     duration: 0.2,
 })
 
-tl.from("h1",{
-    x:100,
+tl.from("h1", {
+    x: 100,
     opacity: 0,
-    stagger:0.07,
+    stagger: 0.07,
 })
 
 
-tl.from(close,{
-    x:50,
+tl.from(close, {
+    x: 50,
     opacity: 0,
 })
 
 tl.pause()
 
-opon.addEventListener("click", ()=>{
+opon.addEventListener("click", () => {
     tl.play()
 })
- 
 
-close.addEventListener("click", ()=>{
+
+close.addEventListener("click", () => {
     tl.reverse()
 })
